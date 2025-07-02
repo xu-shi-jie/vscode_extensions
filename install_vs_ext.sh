43ca1823adacde470f79f2db09f7bf34d74703f8
Shijie <64122236+xu-shi-jie@users.noreply.github.com>
2025-07-02 22:50:22 +0900

    First commit

---
+++ install_vs_ext.sh
@@ -0,0 +1,32 @@
+set -e # I do not like these vscode extensions:
+code --uninstall-extension streetsidesoftware.code-spell-checker --force 
+code --uninstall-extension xirider.livecode --force
+code --uninstall-extension mintlify.document --force 
+code --uninstall-extension ziyasal.vscode-open-in-github --force 
+code --uninstall-extension donjayamanne.git-extension-pack --force
+code --uninstall-extension alefragnani.project-manager --force 
+code --uninstall-extension sourcery.sourcery --force 
+code --uninstall-extension littlefoxteam.vscode-python-test-adapter --force
+code --uninstall-extension hbenl.vscode-test-explorer --force
+code --uninstall-extension ms-vscode.test-adapter-converter --force 
+code --uninstall-extension traBpUkciP.wolf --force
+code --uninstall-extension deerawan.vscode-dash --force
+code --uninstall-extension almenon.arepl --force
+code --uninstall-extension dongli.python-preview --force
+code --install-extension ms-python.python
+code --install-extension GitHub.copilot
+code --install-extension codezombiech.gitignore
+code --install-extension James-Yu.latex-workshop
+code --install-extension ms-python.autopep8
+code --install-extension mutantdino.resourcemonitor
+code --install-extension yohan-pg.nvidia-smi-memory
+code --install-extension ms-vscode.cpptools
+code --install-extension ms-vscode.cpptools-extension-pack
+code --install-extension REditorSupport.r
+code --install-extension matthiasschedel.bibtex-manager
+code --install-extension ms-toolsai.jupyter
+code --install-extension eamodio.gitlens
+code --install-extension oderwat.indent-rainbow
+code --install-extension ms-azuretools.vscode-docker
+code --install-extension yzane.markdown-pdf
+code --install-extension mhutchie.git-graph
